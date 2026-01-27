bbdd8d3430224dfa669fb718f0fe67efea2c87b6
Michael Rozenblat <michael.rozenblat@pendo.io>
2026-01-27 14:00:31 -0500

    Initial Commit

---
+++ Rumble_Hang/Rumble_HangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rumble_HangApp.swift
+//  Rumble_Hang
+//
+//  Created by Michael Rozenblat on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Rumble_HangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
